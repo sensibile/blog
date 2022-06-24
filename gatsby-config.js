ff22f5be5e9ebfedf2b50d0fd42737c3bfbe7f1c
sensibile <wjjung@g.skku.edu>
2022-06-24 20:45:20 +0900

    change site name

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Casper',
-    description: 'A port of the casper blog built for gatsby',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
+    title: 'Sensibile\'s blog',
+    description: '개발 blog with gatsby',
+    siteUrl: 'https://sensibile.netlify.com', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml.name',
